c0fb374887aa5cab67fc383721e476ccea4c6312
chrisho251 <hoduyhoang250196@gmail.com>
2022-08-06 11:01:42 +0700

    Initial Commit

---
+++ MangaList/MangaListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangaListApp.swift
+//  MangaList
+//
+//  Created by Hoang, Ho Duy on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MangaListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
